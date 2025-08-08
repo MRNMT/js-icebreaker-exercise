4c8f6d3e9cfc5575d99c22d138c1c3aa18067661
MRNMT <nelsonmadileng57@gmail.com>
2025-08-08 14:55:48 +0200

    Updated quest-log.js

--- quest-log.js
+++ quest-log.js
@@ -22,20 +22,4 @@ console.log("Completed quest:", completedQuest);
 console.log("Updated quest log:", questLog);
 
 
-questLog.unshift("Rescue the Blacksmith's Cat");
-console.log("After adding an urgent quest:", questLog);
 
-
-let delegatedQuest = questLog.shift();
-console.log("Delegated quest:", delegatedQuest);
-console.log("Updated quest log:", questLog);
-
-questLog.splice(1, 1); 
-console.log("After removing a quest:", questLog);
-
-
-questLog.splice(1, 0, "Slay the Dragon of Mount Doom");
-console.log("After adding a new quest:", questLog);
-
-let questSummary = questLog.join(" | ");
-console.log("Quest summary for the king:", questSummary);
