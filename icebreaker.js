275bd7e2fd2a4b99429ee9661b3ee8887e710e30
Ashley-Blu <matsekolengashley28@gmail.com>
2025-08-08 13:09:30 +0200

    successful pull

---
+++ icebreaker.js
@@ -0,0 +1,10 @@
+const favfood = ["pizza", "fries", "donuts"];
+const discountCode = "";
+const PictureURL = "";
+let galasticStarID = 887323456678095872878435764875n;
+
+console.log("For my favfood variable: the values are inside an array and double quotes because they are a list of food that falls under one variable and they are string values."+
+    "For discountCode: its empty string because there is not one and codes may have charcters an dnumbers mixed."+
+    "PictureURL: This string value is also not available"+
+    "galasticStarID: I put n at the end show that the number is continuing because it is just so long."
+)
