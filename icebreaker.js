dc8478c8b1ed7c6f9492a12fbe9ce0f35a59b3ff
Ashley-Blu <matsekolengashley28@gmail.com>
2025-08-08 13:38:48 +0200

    Added a questLog Javascript file of arrays

--- icebreaker.js
+++ icebreaker.js
@@ -18,7 +18,7 @@ const discountCode = "";
 const PictureURL = "";
 let galasticStarID = 887323456678095872878435764875n;
 
-console.log("My top 3 favourate foods are "+favfood[0], favfood[1], +'and'+ favfood[2] +
+console.log("My top 3 favourate foods are "+favfood[0]+","+ favfood[1]+"," +'and'+ favfood[2] +
     "For discountCode: its empty string because there is not one and codes may have charcters an dnumbers mixed."+
     "PictureURL: This string value is also not available"+
     "galasticStarID: "+galasticStarID+" I put n at the end show that the number is continuing because it is just so long."
---
+++ quest-log.js
@@ -0,0 +1,3 @@
+const questLog = ["Collect 10 Firefly Wings", "Defeat the Goblin King", "Find the Lost Sword of Oakhaven"]
+
+console.log(questLog)
