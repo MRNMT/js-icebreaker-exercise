1e2c8ade84f5b77a981edd158c697bee93b03a74
Ashley-Blu <matsekolengashley28@gmail.com>
2025-08-08 14:56:56 +0200

    pull request

--- quest-log.js
+++ quest-log.js
@@ -1,3 +1,14 @@
-const questLog = ["Collect 10 Firefly Wings", "Defeat the Goblin King", "Find the Lost Sword of Oakhaven"]
-
+questLog.unshift("Rescue the Blacksmith's Cat")//add the quest at the beggining
 console.log(questLog)
+
+console.log("The removed quest is: "+questLog.shift())//removes first quest
+console.log(questLog)
+
+console.log("The removed quest is: "+questLog.splice(1,1))//removes 1 quest from position 1
+console.log(questLog)
+
+questLog.splice(1,0, "Slay the Dragon of Mount Doom");//adds 1 quest from position 1
+console.log(questLog)
+
+console.log(questLog.join(' | '))
+
